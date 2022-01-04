cc70158d08a3c964912fefb1c2ec20329cfac968
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-04 21:29:23 +0630

    bmi calculator was completed

--- 03_BMI_calculator/main.go
+++ 03_BMI_calculator/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+
 func main() {
-	$END$
+	fmt.Print("Enter your weight (kg): ")
+	weightInput, _ := reader.ReadString('\n')
+
+	fmt.Print("Enter your height (m): ")
+	heightInput, _ := reader.ReadString('\n')
+
+	weightInput = strings.ReplaceAll(weightInput, "\n", "")
+	heightInput = strings.ReplaceAll(heightInput, "\n", "")
+
+	weight,_ := strconv.ParseFloat(weightInput,64)
+	height,_ := strconv.ParseFloat(heightInput,64)
+
+	bmi := (weight / (height * height))
+	fmt.Printf("Your BMI: %.2f",bmi)
+
 }
