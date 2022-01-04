3d71a45a87c1459a699629bee76ae4aefa66b447
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-04 16:04:37 +0630

    Variable in go

---
+++ 02_values_variables_and_types/variable.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		 Declaration + Initialization
+		var greetingText string
+		greetingText = "Hello World!"
+	*/
+
+	/*
+		Initialization
+		var greetingText string = "Hello World!"
+	*/
+
+	// as Type inference in ts
+	greetingText := "Hello World!"
+	fmt.Println(greetingText)
+}
