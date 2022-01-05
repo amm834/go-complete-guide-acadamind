0fe1d7c153d4b670df19727e26a8fcfc2e023226
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-05 18:03:58 +0630

    basic function

--- 04_functions/functions.go
+++ 04_functions/functions.go
@@ -6,8 +6,14 @@ func main() {
 	a := 10
 	b := 20
 	fmt.Println(sum(a, b))
+
+	println("hello world")
 }
 
 func sum(a int, b int) int {
 	return a + b
+}
+
+func println(x string) {
+	fmt.Print(x)
 }
