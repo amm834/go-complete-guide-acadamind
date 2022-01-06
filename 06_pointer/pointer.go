32e12a20b7fbf8488a92c25313b361ddedeea5c7
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-06 18:05:51 +0630

    Advantage of using pointer

--- 06_pointer/pointer.go
+++ 06_pointer/pointer.go
@@ -1,11 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
 	age := 18
 	agePtr := &age
 
-	println(agePtr)
+	fmt.Println(agePtr)
 
-	*agePtr = 19
+	*agePtr = 20
 	println(age)
+
+	doubledAge := double(agePtr)
+	fmt.Printf("Doubled Age = %v\n", doubledAge)
+	fmt.Printf("Changed Original Age = %v\n", age)
+
+}
+
+func double(age *int) int {
+	*age = *age * 2
+	return *age
 }
