cef0f75d460f59618c81ff3af30a07f87c577a69
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-06 18:35:12 +0630

    creating a structure

--- 07_structures/structure.go
+++ 07_structures/structure.go
@@ -1,6 +1,13 @@
 package main
 
-import "time"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+var reader = bufio.NewReader(os.Stdin)
 
 type User struct {
 	fullName  string
@@ -9,5 +16,18 @@ type User struct {
 }
 
 func main() {
+	var user User
+	fullName := getInput("Enter your name: ")
+	birthDate := getInput("Enter yout birthday (DD/MM/YYYY): ")
+	createdAt := time.Now()
 
+	user = User{
+		fullName: fullName, birthDate: birthDate, createdAt: createdAt,
+	}
+}
+
+func getInput(promptText string) string {
+	fmt.Print(promptText)
+	input, _ := reader.ReadString('\n')
+	return input
 }
