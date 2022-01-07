ac90ddf97265b47aba6c647c33b01dd751300ae0
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-08 00:40:24 +0630

    else if and else flows

--- 10_control_structure/main.go
+++ 10_control_structure/main.go
@@ -13,8 +13,12 @@ var reader = bufio.NewReader(os.Stdin)
 func main() {
 	ageInput := getInput("Enter your age: ")
 	age, _ := strconv.Atoi(ageInput)
-	if age >= 18 {
+	if age >= 30 {
+		fmt.Println("You are eligible for senior job")
+	} else if age >= 18 {
 		fmt.Println("Welcome to club")
+	} else {
+		fmt.Println("Sorry, we don't accept a child")
 	}
 }
 
