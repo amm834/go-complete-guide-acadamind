6777f1e526c29f331efab912c4a7a75e81a8f58c
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-08 00:52:25 +0630

    error handling

--- 10_control_structure/main.go
+++ 10_control_structure/main.go
@@ -12,9 +12,17 @@ var reader = bufio.NewReader(os.Stdin)
 
 func main() {
 	ageInput := getInput("Enter your age: ")
-	age, _ := strconv.Atoi(ageInput)
-	if age >= 30 {
+	age, err := strconv.Atoi(ageInput)
+
+	if err != nil {
+		fmt.Println("Invalid Input")
+		return
+	}
+
+	if (age >= 30 && age <= 50) || age >= 60 {
 		fmt.Println("You are eligible for senior job")
+	} else if age >= 50 {
+		fmt.Println("The best age")
 	} else if age >= 18 {
 		fmt.Println("Welcome to club")
 	} else {
