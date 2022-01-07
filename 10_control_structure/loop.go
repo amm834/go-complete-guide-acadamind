cae38ad89e75fae3e61e3c76634022fce6a238e0
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-08 02:42:47 +0630

    some functional was implemented myself

--- 10_control_structure/loop.go
+++ 10_control_structure/loop.go
@@ -5,46 +5,116 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
+var reader = bufio.NewReader(os.Stdin)
+
 func main() {
-	userChoice, err := getUserInput()
+	input, err := getUserInput()
+
 	if err != nil {
-		fmt.Println("Invalid Choice Occured! Exiting!")
+		fmt.Println(err)
+		return
 	}
-
-	if userChoice == "1" {
-
-	} else if userChoice == "2" {
-
-	} else if userChoice == "3" {
-
-	} else if userChoice == "4" {
+	if input == "1" {
+		calculateUntilNumber()
+	} else if input == "2" {
+		calculateFactorial()
+	} else if input == "3" {
+		calculateUserEnteredNumber()
+	} else if input == "4" {
 
 	}
+}
 
+func showMenuList() {
+	fmt.Printf(`
+	Avaliable: 
+		1) Sum up to x
+		2) Sum factoial x
+		3) Sum by Entering Numbers
+`)
 }
 
 func getUserInput() (string, error) {
-	fmt.Println("1) Sum up to X: ")
-	fmt.Println("2) Sum factorial X: ")
-	fmt.Println("3) Sum up manually entered numbers: ")
-	fmt.Println("4) Sum up a list of entered numbers: ")
+	showMenuList()
+	fmt.Print("Enter your choice: ")
 
-	var reader = bufio.NewReader(os.Stdin)
 	userInput, err := reader.ReadString('\n')
+
+	userInput = strings.ReplaceAll(userInput, "\n", "")
 	if err != nil {
 		return "", err
 	}
-	if userInput == "1" ||
-		userInput == "2" ||
-		userInput == "3" ||
-		userInput == "4" {
 
+	if userInput == "1" || userInput == "2" || userInput == "3" || userInput == "4" {
 		return userInput, nil
-
 	} else {
-		return "", errors.New("INVALID INPUT")
+		return "", errors.New("Invalid User Input Error")
+	}
+}
+
+func calculateUntilNumber() {
+	fmt.Print("Enter Your Number: ")
+	inputNumber, err := getUserInputNumber()
+
+	if err != nil {
+		fmt.Println("Invalid Number Input")
+		return
 	}
 
+	result := 0
+	for i := 0; i <= inputNumber; i++ {
+		result += i
+	}
+
+	fmt.Printf("Total Result: %d", result)
+
+}
+
+func calculateFactorial() {
+	fmt.Print("Enter Your Number: ")
+	inputNumber, err := getUserInputNumber()
+
+	if err != nil {
+		fmt.Println("Invalid Number Input")
+		return
+	}
+
+	fmt.Printf("Total Number of factorial %d: %d", inputNumber, factorial(inputNumber))
+}
+
+func getUserInputNumber() (int, error) {
+	numberInput, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+	numberInput = strings.ReplaceAll(numberInput, "\n", "")
+
+	number, err := strconv.Atoi(numberInput)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
+
+func factorial(number int) int {
+	if number <= 1 {
+		return 1
+	}
+	return number * factorial(number-1)
+}
+
+func calculateUserEnteredNumber() {
+	isEntering := true
+	sum := 0
+	for isEntering {
+		fmt.Print("Enter your number: ")
+		input, err := getUserInputNumber()
+		isEntering = err == nil
+		sum += input
+	}
+	fmt.Printf("Total Number: %d", sum)
 }
