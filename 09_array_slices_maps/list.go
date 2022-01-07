1ff31bf818be6efb4f8f8dfb6507633c2f04efa4
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-07 23:06:08 +0630

    accessing array elements

--- 09_array_slices_maps/list.go
+++ 09_array_slices_maps/list.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	var names [4]string
 	var prices = []float64{10.2, 20.3, 20, 20}
+
+	names = [4]string{
+		"Mg Mg", "Hla Hla", "Aung Aung", "Moe Moe",
+	}
+
+	prices[2] = 400
 	fmt.Println(prices)
+	fmt.Println(names)
 }
