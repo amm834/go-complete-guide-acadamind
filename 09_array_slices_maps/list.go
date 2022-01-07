ae94e03329a00cb7233a3518f4b4f8fffe7bf8c3
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-07 23:10:45 +0630

    array slices

--- 09_array_slices_maps/list.go
+++ 09_array_slices_maps/list.go
@@ -4,13 +4,17 @@ import "fmt"
 
 func main() {
 	var names [4]string
-	var prices = []float64{10.2, 20.3, 20, 20}
+	var prices = []float64{10.2, 20.3, 20, 30}
 
 	names = [4]string{
 		"Mg Mg", "Hla Hla", "Aung Aung", "Moe Moe",
 	}
 
-	prices[2] = 400
 	fmt.Println(prices)
 	fmt.Println(names)
+
+	fmt.Println(names[0])
+
+	featuredPrices := prices[1:3]
+	fmt.Println(featuredPrices)
 }
